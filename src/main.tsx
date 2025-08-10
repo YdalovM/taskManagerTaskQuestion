91bbe95783305e7024b06efe1f99943efdf13a64
Михаил Удалов <7mishka29@gmail.com>
2025-08-10 10:55:28 +0300

    Fix React Router routing: add basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/taskManagerTaskQuestion">
       <App />
     </BrowserRouter>
   </StrictMode>
